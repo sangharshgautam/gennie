6007622414333aa345c873a4911492c990beab7e
Sangharsh Gautam# <sangharsh_iitd@yahoo.com>
2015-11-16 12:00:10 +0000

    Trigger

--- src/main/java/uk/co/sangharsh/ws/resource/AdminResource.java
+++ src/main/java/uk/co/sangharsh/ws/resource/AdminResource.java
@@ -17,28 +17,27 @@ import uk.co.sangharsh.service.TelegramService;
 
 @Component
 @Path(AdminResource.ROOT)
-@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 @Consumes(MediaType.APPLICATION_JSON)
 public class AdminResource {
 	public static final String ROOT = "admin";
 	private static final String NOTIFY = "notify";
-	
+
 	final String ADMIN_CHAT_ID = "120340564";
-	
+
 	@Autowired
 	private TelegramService telegramService;
-	
-	
+
 	@GET
 	@Path("message/{chatId}")
-	public Telegram getMe(@PathParam("chatId") String chatId, @QueryParam("msg") String message){
+	public Telegram getMe(@PathParam("chatId") String chatId, @QueryParam("msg") String message) {
 		MessageResult result = telegramService.message(chatId, message);
 		return result.getResult();
 	}
-	
+
 	@GET
 	@Path(NOTIFY)
-	public Telegram getMe(@QueryParam("msg") String message){
+	public Telegram getMe(@QueryParam("msg") String message) {
 		MessageResult result = telegramService.message(ADMIN_CHAT_ID, message);
 		return result.getResult();
 	}
