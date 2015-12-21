b869c5d7786f43d9b8873d2bcc6a50e65e9bdf09
Sangharsh Gautam <sangharsh_iitd@yahoo.com>
2015-12-22 00:22:39 +0530

    Redirect handler

--- src/main/java/uk/co/sangharsh/ws/resource/SlackResource.java
+++ src/main/java/uk/co/sangharsh/ws/resource/SlackResource.java
@@ -1,9 +1,12 @@
 package uk.co.sangharsh.ws.resource;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Component;
 
@@ -13,4 +16,11 @@ import org.springframework.stereotype.Component;
 @Consumes(MediaType.APPLICATION_JSON)
 public class SlackResource {
 	public static final String ROOT = "slack";
+	
+	@GET
+	@Path("authorize/handle")
+	public Response redirected(@QueryParam(value = "code") String code){
+		System.out.println("CODE: "+code);
+		return Response.ok().build();
+	}
 }
